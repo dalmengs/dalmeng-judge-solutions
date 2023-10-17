45c0ecc9b0e507aa2996774d61201fee5f86f953
dalmengs <125876190+dalmengs@users.noreply.github.com>
2023-10-17 20:06:07 +0900

    Create 26190068-6cdd-11ee-b962-0242ac120002.py

---
+++ 26190068-6cdd-11ee-b962-0242ac120002.py
@@ -0,0 +1,25 @@
+def decimalToHex(n, base = 16):
+    ret = []
+        
+    while n:
+        ret.append(n % base)
+        n //= base
+        
+    return "".join(reversed(list(map(str, ret))))
+
+n = int(input())
+s = decimalToHex(n)
+n = len(s)
+
+dp = [[-1 for j in range(n + 5)] for i in range(16)]
+ans = dp[0][len(s) - 1] = int(s[-1])
+
+for i in range(1, 16):
+    for j in range(len(s) - 1, -1, -1):
+        if dp[i - 1][j + 1] != -1:
+            dp[i][j] = max(dp[i][j], (dp[i - 1][j + 1] + 16**i * int(s[j])))
+        if dp[i - 1][j + 2] != -1:
+            dp[i][j] = max(dp[i][j], (dp[i - 1][j + 2] + 16**i * int(s[j] + s[j + 1])))
+        ans = max(ans, dp[i][j])
+
+print(ans)
