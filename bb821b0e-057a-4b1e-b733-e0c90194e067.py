09b4be012b0d608ed23efdd762c3a30124e6ca5c
달맹 Dalmeng <125876190+dalmengs@users.noreply.github.com>
2023-12-02 17:14:05 +0900

    Create bb821b0e-057a-4b1e-b733-e0c90194e067.py

---
+++ bb821b0e-057a-4b1e-b733-e0c90194e067.py
@@ -0,0 +1,32 @@
+n = int(input())
+
+synonym = {}
+summary = []
+
+for _ in range(n):
+    q = input().split()
+    
+    k = int(q[0])
+    for j in q[1:]:
+        synonym[j.lower()] = len(summary)
+    
+    t = int(input())
+    temp = []
+    for __ in range(t):
+        temp.append(input())
+    summary.append(temp)
+
+t = int(input())
+for _ in range(t):
+    q = input().split()
+    
+    k = int(q[0])
+    if k == 1:
+        x, y = q[1].lower(), q[2]
+        synonym[y] = synonym[x]
+    elif k == 2:
+        x, y = q[1].lower(), " ".join(q[2:])
+        summary[synonym[x]].append(y)
+    else:
+        for i in summary[synonym[q[1].lower()]]:
+            print(i)
