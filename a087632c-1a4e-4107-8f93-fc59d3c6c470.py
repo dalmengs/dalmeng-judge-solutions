bf2de06592e685f583e9a5be6c54ef4078081979
달맹 Dalmeng <125876190+dalmengs@users.noreply.github.com>
2024-02-09 22:15:01 +0900

    Update a087632c-1a4e-4107-8f93-fc59d3c6c470.py

--- a087632c-1a4e-4107-8f93-fc59d3c6c470.py
+++ a087632c-1a4e-4107-8f93-fc59d3c6c470.py
@@ -1,9 +1,10 @@
 import sys
-sys.setrecursionlimit(int(1e7))
+sys.setrecursionlimit(int(1e5))
 
 n = int(input())
-a = list(map(int, input().split()))
+a = [int(input()) for i in range(n)]
 
+ans = max(a)
 tree = [0 for i in range(100005 * 4)]
 
 def initTree(s, e, node):
@@ -14,6 +15,7 @@ def initTree(s, e, node):
     mid = (s + e) // 2
     left = initTree(s, mid, node * 2)
     right = initTree(mid + 1, e, node * 2 + 1)
+    
     if left[0] < right[0]:
         tree[node] = left
     else:
@@ -22,7 +24,7 @@ def initTree(s, e, node):
     
 def getMin(s, e, x, y, node):
     if e < x or y < s:
-        return [987654321, -1]
+        return [987654321, 987654321]
     if x <= s and e <= y:
         return tree[node]
     
@@ -31,27 +33,24 @@ def getMin(s, e, x, y, node):
     right = getMin(mid + 1, e, x, y, node * 2 + 1)
     
     if left[0] < right[0]:
-        tree[node] = left
+        return left
     else:
-        tree[node] = right
-    return tree[node]
-
-ans = 0
+        return right
+        
 initTree(0, n - 1, 1)
 
 def dfs(s, e):
     global ans
-    if s >= e:
-        return
     
     m = getMin(0, n - 1, s, e, 1)
     ans = max(ans, (e - s + 1) * m[0])
     
     mid = m[1]
     
-    dfs(s, mid - 1)
-    dfs(mid + 1, e)
+    if s < mid - 1:
+        dfs(s, mid - 1)
+    if mid + 1 < e:
+        dfs(mid + 1, e)
 
 dfs(0, n - 1)
 print(ans)
-    
